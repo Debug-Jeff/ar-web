749048fe91cb70452a74dba205a8f3aed61f5a78
Jeff <jmutugi00.0@gmail.com>
2024-12-21 12:31:53 +0300

    Further changes to index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
+
+// Initialize tooltips
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// Initialize popovers
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach(popoverTriggerEl => {
+    new bootstrap.Popover(popoverTriggerEl);
+});
